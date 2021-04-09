75e9c4258366628b12ad5c97e83f6145710a30d0
reyansh <yash.dewangan82@gmail.com>
2021-04-09 20:38:26 +0100

    1st commit

---
+++ src/main/java/com/yashdew/App.java
@@ -0,0 +1,13 @@
+package com.yashdew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is Yashpal Dewangan - 2021");
+    }
+}
